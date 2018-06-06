de6b5e31a0ad02832ed357d37320e07c02816dfb
Kostiantyn Burlai <kostiantyn.burlai@gmail.com>
2018-06-06 23:53:21 +0200

    add oauth - start

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ dupa: 'kupa' });
-});
+// clientId: 213413296626-ne4m4982cg7j83cvsq7r65je5rmuvje2.apps.googleusercontent.com
+// clientSecret: HLJGrCmg5uULFaixBkVCwBeD
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
